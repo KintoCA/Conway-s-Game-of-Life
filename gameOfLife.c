a4a5ad8efa8c27b6b3d9bc66c22ed5cde5124459
Andrey Varshavsky <andrey@varshavsky.org>
2024-03-03 15:16:45 -0500

    Comments

--- gameOfLife.c
+++ gameOfLife.c
@@ -1,3 +1,8 @@
+/*
+    Works in Linux (Mint 21/gcc-13) and Windows (cl.exe 19.38)
+    https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,10 +15,10 @@
 #ifdef _WIN32
 #include <windows.h>
 #ifdef _MSC_VER
-#pragma warning(disable : 4996) // Ignore specific deprecation warnings
+#pragma warning(disable : 4996)
 #endif
 void usleep(__int64 usec) {
-    Sleep((DWORD)(usec/1000)); // Windows version
+    Sleep((DWORD)(usec/1000));
 }
 #else
 #include <unistd.h>
@@ -21,13 +26,18 @@ void usleep(__int64 usec) {
 
 uint64_t bit = 0x8000000000000000;
 
-bool is_alive(uint64_t board, uint8_t cId)
-{
+extern inline bool is_alive(uint64_t board, uint8_t cId);
+extern inline int count_neighbors(uint64_t board, uint8_t cId);
+extern inline void set_alive(uint64_t* board, uint8_t cid);
+extern inline void set_dead(uint64_t* board, uint8_t cid);
+extern inline void print_board(uint64_t board);
+extern inline uint64_t update(uint64_t board);
+
+inline bool is_alive(uint64_t board, uint8_t cId){
     return board & (bit >> cId);
 }
 
-int count_neighbors(uint64_t board, uint8_t cId)
-{
+inline int count_neighbors(uint64_t board, uint8_t cId){
     cId = (cId % 8) * 8 + cId / 8;
     int count = 0;
     count += is_alive(board, (cId + 64 + 1) % 64) != 0;
@@ -41,20 +51,16 @@ int count_neighbors(uint64_t board, uint8_t cId)
     return count;
 }
 
-void set_alive(uint64_t* board, uint8_t cid)
-{
+inline void set_alive(uint64_t* board, uint8_t cid){
     *board |= (bit >> (cid % 64));
 }
 
-void set_dead(uint64_t* board, uint8_t cid)
-{
+inline void set_dead(uint64_t* board, uint8_t cid){
     *board &= ~(bit >> (cid % 64));
 }
 
-void print_board(uint64_t board)
-{
-    for (int i = 1; i < 65; i++)
-    {
+inline void print_board(uint64_t board){
+    for (int i = 1; i < 65; i++){
         printf("%c ", board & (bit >> (i - 1)) ? 'X' : '.');
         if (!(i % 8))
             putchar(10);
@@ -62,24 +68,18 @@ void print_board(uint64_t board)
     putchar(10);
 }
 
-uint64_t update(uint64_t board)
-{
+inline uint64_t update(uint64_t board){
     uint64_t new_board = 0;
-    for (int i = 0; i < 64; i++)
-    {
+    for (int i = 0; i < 64; i++){
         int nc = ((i % 8) * 8 + i / 8) % 64;
         int live_count = count_neighbors(board, nc);
-        if (is_alive(board, i))
-        {
-            if (live_count == 2 || live_count == 3)
-            {
+        if (is_alive(board, i)){
+            if (live_count == 2 || live_count == 3){
                 set_alive(&new_board, i);
             }
         }
-        else
-        { // Cell is dead
-            if (live_count == 3)
-            {
+        else{
+            if (live_count == 3){
                 set_alive(&new_board, i);
             }
         }
@@ -95,37 +95,36 @@ void convertToUpper(char* str) {
         str++;
     }
 }
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
+
     const uint64_t bLinNkEr = 0x808080000;
     const uint64_t BEACON = 0x3020040c0000;
     const uint64_t TOAD = 0x1c380000;
-    char mode[10] = "RANDOM"; // Default value, ensure the array is large enough
+    char mode[10] = "RANDOM";
     time_t rawtime;
     time(&rawtime);
     uint64_t board = rawtime;
 
-    if (argc > 1 && argv[1] != NULL) { // Check if there are command line arguments and they are not NULL
-        char argUpper[100]; // Buffer for the uppercase argument, size should be sufficient for expected input
+    if (argc > 1 && argv[1] != NULL) {
+        char argUpper[100];
 
-        strncpy(argUpper, argv[1], sizeof(argUpper)); // Safely copy the argument
-        argUpper[sizeof(argUpper) - 1] = '\0'; // Ensure null-termination
-        convertToUpper(argUpper); // Convert the copied argument to uppercase
+        strncpy(argUpper, argv[1], sizeof(argUpper));
+        argUpper[sizeof(argUpper) - 1] = '\0'; 
+        convertToUpper(argUpper);
 
         if (strcmp(argUpper, "BEACON") == 0) {
-            strncpy(mode, "BEACON", sizeof(mode)); // Safe copy
+            strncpy(mode, "BEACON", sizeof(mode));
             board = BEACON;
         }
         else if (strcmp(argUpper, "BLINKER") == 0) {
-            strncpy(mode, "BLINKER", sizeof(mode)); // Safe copy
+            strncpy(mode, "BLINKER", sizeof(mode));
             board = bLinNkEr;
         }
         else if (strcmp(argUpper, "TOAD") == 0) {
-            strncpy(mode, "TOAD", sizeof(mode)); // Safe copy
+            strncpy(mode, "TOAD", sizeof(mode));
             board = TOAD;
         }
-        // No need to check for "RANDOM" as it's already the default
-        mode[sizeof(mode) - 1] = '\0'; // Ensure null-termination
+        mode[sizeof(mode) - 1] = '\0';
     }
 
     printf("%s\n", mode);
@@ -142,3 +141,4 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
+
